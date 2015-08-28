6614abfa7e4f5c03737c5b55964a094efbe903cc
JanneLindberg <janne-lindberg@bahnhof.se>
2015-08-28 23:22:05 +0200

    Added Util java class

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :javac-options ["-source" "1.7" "-target" "1.7" "-g"]
+  :java-source-paths ["src/java"]
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]
                  ])

f952a3c4f2ab1a8284e4c6e794d731b5d0bdde72
JanneLindberg <janne-lindberg@bahnhof.se>
2015-08-29 23:58:57 +0200

    Build with travis

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject telldus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure library for communication with Telldus Tellstick daemon"
+  :url "https://github.com/JanneLindberg/clj-telldus.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -9,4 +9,8 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]
-                 ])
+                 ]
+  :profiles {
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             })
